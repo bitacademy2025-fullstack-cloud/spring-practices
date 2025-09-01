7c3ab4289a47a85fee68a9b3595646e2e5efc54d
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-09-01 15:53:36 +0900

    spring practice #2

--- hellospring/src/main/java/com/bit2025/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/bit2025/hellospring/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.bit2025.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -10,4 +12,20 @@ public class HelloController {
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(@RequestParam("n") String name) {
+		System.out.println("name:" + name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(@RequestParam("n") String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		mav.addObject("name", name);
+		
+		return mav;
+	}
+	
 }
