ff8e987e00e4fa62522594f9a177421203a70dc0
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-09-10 17:10:37 +0900

    file upload #finish

--- fileupload/src/main/java/com/bit2025/fileupload/controller/FileuploadController.java
+++ fileupload/src/main/java/com/bit2025/fileupload/controller/FileuploadController.java
@@ -1,13 +1,19 @@
 package com.bit2025.fileupload.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.bit2025.fileupload.service.FileuploadService;
+
 @Controller
 public class FileuploadController {
+	
+	@Autowired
+	private FileuploadService fileuploadService;
 	
 	@RequestMapping("/")
 	public String form() {
@@ -17,7 +23,7 @@ public class FileuploadController {
 	@RequestMapping("/upload")
 	public String upload(
 		@RequestParam(value="email", required=true, defaultValue="") String email,
-		@RequestParam(value="file") MultipartFile multipartFiles,
+		@RequestParam(value="file") MultipartFile multipartFile,
 		Model model) {
 		System.out.println("email:" + email);
 		
