9a20b75219cdd07afeb0c307fb8479785c444e23
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-09-15 12:59:52 +0900

    java config #2

---
+++ bean-wiring/src/main/java/config/videosystem/DVDPlayerConfig.java
@@ -0,0 +1,40 @@
+package config.videosystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import videosystem.Avengers;
+import videosystem.DVDPlayer;
+import videosystem.DigitalVideoDisc;
+import videosystem.IronMan;
+
+@Configuration
+public class DVDPlayerConfig {
+	
+	@Bean
+	public DigitalVideoDisc avengers() {
+		return new Avengers();
+	}
+
+	@Bean
+	public DigitalVideoDisc ironMan() {
+		return new IronMan();
+	}
+	
+	// 의존성 주입(Dependency Injection) 하기 1
+	// 1. Bean 생성 메소드를 직접 호출하는 방법
+	// 2. 생성자 주입 
+	@Bean
+	public DVDPlayer dvdPlayer1() {
+		return new DVDPlayer(avengers());
+	}
+	
+	// 의존성 주입(Dependency Injection) 하기 1
+	// 1. 파라미터로 Bean을 전달하는 방법
+	// 2. 생성자 주입 
+	@Bean("dvdPlayer2nd")
+	public DVDPlayer dvdPlayer2(Avengers dvd) {
+		return new DVDPlayer(dvd);
+	}
+	
+}
